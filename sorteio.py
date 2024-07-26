6b46867bf006f3241aa306b860046484aadaec3e
Élaff Ramos <valelaff10@gmail.com>
2024-07-26 18:46:18 -0300

    Create sorteio.py

---
+++ sorteio.py
@@ -0,0 +1,11 @@
+import random
+
+print('\nSORTEIO\n')
+
+começo = int(input('Digite de onde vai começar o sorteio: '))
+fim = int(input('Digite o último número do sorteio: '))
+quantidade = int(input('Digite quantos números você deseja: '))
+
+numeros = range(começo, fim)
+sorteio = random.sample(numeros, quantidade)
+print('\nO número sorteado é: {}\n'.format(sorteio))
